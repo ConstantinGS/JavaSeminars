8451136e24bf313bf1eb70538dc4128e0357011d
Constantin <grishanoff.konstantin@yandex.ru>
2022-09-06 15:43:36 +0500

    Compl

---
+++ sem_J_1/calculator.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+public class calculator {
+
+    public static void main( String[] args ) {
+        
+        Scanner iScanner = new Scanner(System.in, "cp866");
+        
+        
+            boolean merker = false;
+            while (merker!=true){ 
+                System.out.println("Калькулятор");
+                System.out.println("Введите первое число");
+                String firstNumber = iScanner.nextLine();
+                int fNumber = Integer.parseInt(firstNumber);
+                System.out.println("Введите второе число");
+                String secondNumber = iScanner.nextLine();
+                int sNumber = Integer.parseInt(secondNumber);
+                System.out.println("Введите желаемую операцию");
+                String operation = iScanner.nextLine();
+
+                switch(operation) {
+                    case("+"):
+                        System.out.println("Результат:" + (fNumber+sNumber));
+                        break;
+                    case("-"):
+                        System.out.println("Результат:" + (fNumber-sNumber));
+                        break;
+                    case("*"):
+                        System.out.println("Результат:" + (fNumber*sNumber));
+                        break;
+                    case("/"):
+                        System.out.println("Результат:" + (fNumber/sNumber));
+                        break;
+
+                }
+
+                System.out.println("Желаете продолжить?");
+                String cont = iScanner.nextLine();
+                if (cont.toLowerCase().equals("нет")) merker = true;
+
+        }
+
+
+}}
