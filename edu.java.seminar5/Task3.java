fde5082dbcbfe4c9e4b8d3fe6f63462e7de2939a
Constantin <grishanoff.konstantin@yandex.ru>
2022-09-23 15:50:14 +0500

    1,2 compl

--- edu.java.seminar5/Task3.java
+++ edu.java.seminar5/Task3.java
@@ -1,3 +1,29 @@
 public class Task3 {
+
+    public static void main( String[] args ) {
+       
+        int[] array = new int[] {10, 5, 6, 7, 1, 1, 11, 20};
+        
+    }
+
+    public static int[] heapSort(int[] array){
+
+
+
+        return array;
+    }
+
+    public static int minElement(int[] myarray){
+
+        return 1;
+    }
+
+    public static int[] sortirovka(int[] myarray){
+
+        return null;
+    }
+
+
+
     
 }
